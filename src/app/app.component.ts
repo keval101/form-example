bc10305b2ffb3537407244679a3b8aa7f59f5f0e
keval101 <kevalvadhiya101@gmail.com>
2021-07-16 14:23:07 +0530

    onSubmit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  @ViewChild('f') form:NgForm
+
+  user:{username:string, email:string, secret:string}[] = [];
+  suggestUserName(){
+    const suggestUsername = 'Super User';
+  }
+
+  onSubmit(){
+    console.log(this.form)
+
+    if(this.form.valid === true){
+      console.log('Form Submited !')
+    } else {
+      alert('Invalid Detail !')
+    }
+  }
+  // onSubmit(f:NgForm){
+  //   console.log(f.value)
+  //   this.user.push(f.value.username, f.value.email,f.value.secret)
+  //   console.log(this.user)
+  // }
 }
